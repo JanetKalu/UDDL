acdefc76383f6e7993ed4da7e2d584904f420d80
Steve Hickman <steve@epistimis.com>
2022-11-30 13:24:27 -0800

    Fix warning - and add comments / TODO in Formatter

--- com.epistimis.uddl/src/com/epistimis/uddl/formatting2/UddlFormatter.java
+++ com.epistimis.uddl/src/com/epistimis/uddl/formatting2/UddlFormatter.java
@@ -12,7 +12,41 @@ import org.eclipse.xtext.formatting2.AbstractJavaFormatter;
 import org.eclipse.xtext.formatting2.IFormattableDocument;
 
 public class UddlFormatter extends AbstractJavaFormatter {
+	
+	/**
+	 * Several standard methods needed for formatting
+	 * 1) All objects will have 
+	 * 		A) '{' and '};' on a newline. 
+	 * 		B) All content inside the '{' and '}' will be indented one tab
+	 * 
+	 * NOTES: Because we don't know the structure of the object, we don't know how to format its
+	 * content. So we format the open and close separately
+	 * 
+	 * 2) '{' and '}' for scoping means
+	 * 		A) all content indented one tab further
+	 * 		B) '{' on newline after name/description of scope
+	 * 
+	 * 3) '[' and ']' for lists means
+	 * 		A) all list content have 1 space between
+	 * 		B) '[', ']' and content all on the same line
+	 * 
+	 * 
+	 */
+	protected <O> void formatObjOpen(O obj, IFormattableDocument doc) {
+		
+	}
+	protected <O> void formatObjClose(O obj, IFormattableDocument doc) {
+		
+	}
+	
+	protected <L> void formatList(L list, IFormattableDocument doc) {
+		
+	}
 
+	protected  <C> void formatContainer(C container, IFormattableDocument doc) {
+		
+	}
+	
 	protected void format(DataModel dataModel, IFormattableDocument doc) {
 		// TODO: format HiddenRegions around keywords, attributes, cross references, etc. 
 		for (ConceptualDataModel conceptualDataModel : dataModel.getCdm()) {
